d512ec7803dfe444b02c913b25bea27ca56cd5bc
Peter Sharpe <pds@mit.edu>
2021-09-15 11:47:38 -0400

    psd working

--- Gusts_MMS/analysis_preliminary/fourier_analysis.py
+++ Gusts_MMS/analysis_preliminary/fourier_analysis.py
@@ -1,7 +1,26 @@
 from load_single_file import data
 import numpy as np
-from scipy import fft
+from scipy import fft, signal
 
-out = fft.fft(data[])
+w = data["W"].values
+N = len(w)
+
+w_windowed = w * signal.windows.hann(N)
+
+freq = fft.fftfreq(N, 1/20)
+out = fft.fft(w_windowed)
+
+freq = freq[:N//2]
+out = out[:N//2]
+
+freq = freq / (2 * np.pi)
+
+from aerosandbox.tools.pretty_plots import plt, show_plot
+plt.loglog(freq, np.abs(out) ** 2)
+show_plot(
+    "Power Spectral Density",
+    "Frequency [Hz]",
+    "Power Spectral Density"
+)
 
 print(out)
