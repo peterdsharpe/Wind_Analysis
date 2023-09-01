773ac3f7f13cfa11277f62b017f8e6f54ae68846
Peter Sharpe <peterdsharpe@gmail.com>
2023-09-01 15:42:47 -0400

    modernize plotting

--- CONUS/Gusts/statistical_models/von_karman_psds.py
+++ CONUS/Gusts/statistical_models/von_karman_psds.py
@@ -49,10 +49,8 @@ if __name__ == '__main__':
 
     ### Plot PSDs
     import matplotlib.pyplot as plt
-    import seaborn as sns
-
-    sns.set(palette=sns.color_palette("husl"))
-    fig, ax = plt.subplots(1, 1, figsize=(6.4, 4.8), dpi=200)
+    import aerosandbox.tools.pretty_plots as p
+    fig, ax = plt.subplots(figsize=(6.4, 4.8), dpi=200)
     plt.xscale('log')
     plt.yscale('log')
     psds = spatial_psds(
@@ -60,9 +58,9 @@ if __name__ == '__main__':
         altitude=altitude,
         probability_of_exceedance=probability_of_exceedance
     )
-    plt.plot(Omega, psds[0], label=r"$\Phi_u$")
-    plt.plot(Omega, psds[1], label=r"$\Phi_v$")
-    plt.plot(Omega, psds[2], label=r"$\Phi_w$")
+    plt.plot(Omega, psds[0], label=r"$\Phi_u$", alpha=0.7)
+    plt.plot(Omega, psds[1], label=r"$\Phi_v$", alpha=0.7)
+    plt.plot(Omega, psds[2], label=r"$\Phi_w$", alpha=0.7)
     plt.annotate(
         text="Data: \"Flying Qualities of Piloted Aircraft\"\nMIL-STD-1797A (1990), U.S. DoD",
         xy=(0.02, 0.02),
@@ -72,12 +70,9 @@ if __name__ == '__main__':
         fontsize=8,
         color=(0, 0, 0, 1)
     )
-    plt.axis("equal")
+
     plt.xlabel(r"Spatial Frequency [rads/meter]")
     plt.ylabel(r"Gust Power Spectral Density ($\Phi$) [$m^3/s^2$]")
     plt.title(r"Von Karman Gusts: Power Spectral Density Models"
               f"\n{altitude} m Altitude, $10^{'{'}{np.log10(probability_of_exceedance):.0f}{'}'}$ Prob. of Exceedance")
-    plt.tight_layout()
-    plt.legend()
-    # plt.savefig("C:/Users/User/Downloads/temp.svg")
-    plt.show()
+    p.show_plot()
